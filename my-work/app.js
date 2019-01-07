5b53ac0f2b2f6b5d5a56311e61cacff5a8e153a4
JuSeonghyeok <shrewdact@gmail.com>
2019-01-07 15:15:11 +0900

    Done 1.3.5 javascript method chaining(mainly about .enter() method)

--- my-work/app.js
+++ my-work/app.js
@@ -1,3 +1,11 @@
-// d3.select("circle").attr("class", "tentative");
-d3.select('circle').classed('active', true)
-console.log("hi")
+var someData = ['filler', 'filler', 'filler', 'filler'];
+
+d3.select('body')
+  .selectAll('div')
+  .data(someData)
+  .enter()
+  .append('div')
+  .html('Wow')
+  .append('span')
+  .html('Event More Wow')
+  .style('font-weight', 900);
