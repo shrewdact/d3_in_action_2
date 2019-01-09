79f1ed224491607c1d3968338d2c8ca6c46139ec
JuSeonghyeok <shrewdact@gmail.com>
2019-01-09 11:49:02 +0900

    Done 1.3.5 Javascript

--- my-work/app.js
+++ my-work/app.js
@@ -1,11 +1,19 @@
-var someData = ['filler', 'filler', 'filler', 'filler'];
+var someNumbers = [17, 82, 9, 500, 40];
+var smallerNumbers = someNumbers.filter(function(d) {
+  return d <= 40;
+});
+var someColors = ['blue', 'red', 'chartreuse', 'orange'];
+var somePeople = [
+  { name: 'Peter', age: 27 },
+  { name: 'Sulayman', age: 24 },
+  { name: 'K.C.', age: 49 }
+];
 
 d3.select('body')
   .selectAll('div')
-  .data(someData)
+  .data(smallerNumbers)
   .enter()
   .append('div')
-  .html('Wow')
-  .append('span')
-  .html('Event More Wow')
-  .style('font-weight', 900);
+  .html(function(d) {
+    return d;
+  });
