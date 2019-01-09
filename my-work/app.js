798823898a6b508998fdd38e2235ca1d204f9da1
JuSeonghyeok <shrewdact@gmail.com>
2019-01-09 20:04:08 +0900

    Done 2.2.1 selections and binding

--- my-work/app.js
+++ my-work/app.js
@@ -1,9 +1,18 @@
 // d3.csv('../data/cities.csv', data => console.log(data));
-d3.json('../data/tweets.json', data => {
-  var tweetData = data.tweets;
-  var nestedTweets = d3
-    .nest()
-    .key(d => d.user)
-    .entries(tweetData);
-  console.log(nestedTweets)
+d3.csv('../data/cities.csv', (error, data) => {
+  if (error) {
+    console.error(error);
+  } else {
+    dataViz(data);
+  }
 });
+
+function dataViz(incomingData) {
+  d3.select('body')
+    .selectAll('div.cities')
+    .data(incomingData)
+    .enter()
+    .append('div')
+    .attr('class', 'cities')
+    .html(d => d.label);
+}
