0253c2ba7cf08c2e66607a8e890e9127796dc438
JuSeonghyeok <shrewdact@gmail.com>
2019-01-09 12:06:53 +0900

    Done 1.6.2 Hello World with circles (explians about layout ordering)

--- my-work/app.js
+++ my-work/app.js
@@ -1,19 +1,34 @@
-var someNumbers = [17, 82, 9, 500, 40];
-var smallerNumbers = someNumbers.filter(function(d) {
-  return d <= 40;
-});
-var someColors = ['blue', 'red', 'chartreuse', 'orange'];
-var somePeople = [
-  { name: 'Peter', age: 27 },
-  { name: 'Sulayman', age: 24 },
-  { name: 'K.C.', age: 49 }
-];
+d3.select('svg')
+  .append('line')
+  .attr('x1', 20)
+  .attr('x1', 20)
+  .attr('x2', 400)
+  .attr('y2', 400)
+  .style('stroke', 'black')
+  .style('stroke-width', '2px');
 
-d3.select('body')
-  .selectAll('div')
-  .data(smallerNumbers)
-  .enter()
-  .append('div')
-  .html(function(d) {
-    return d;
-  });
+d3.select('svg')
+  .append('text')
+  .attr('x', 20)
+  .attr('y', 20)
+  .text('HELLO');
+
+d3.select('svg')
+  .append('circle')
+  .attr('r', 20)
+  .attr('cx', 20)
+  .attr('cy', 20)
+  .style('fill', 'red');
+
+d3.select('svg')
+  .append('circle')
+  .attr('r', 100)
+  .attr('cx', 400)
+  .attr('cy', 400)
+  .style('fill', 'lightblue');
+
+d3.select('svg')
+  .append('text')
+  .attr('x', 400)
+  .attr('y', 400)
+  .text('WORLD');
