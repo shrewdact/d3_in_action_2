4e68260adbf3e7b59ab85394bb1318daa89b87dd
JuSeonghyeok <shrewdact@gmail.com>
2019-01-09 22:43:28 +0900

    Done 2.2.3 integrating scale

--- my-work/app.js
+++ my-work/app.js
@@ -1,12 +1,24 @@
 // d3.csv('../data/cities.csv', data => console.log(data));
 
+var yScale = d3
+  .scaleLinear()
+  .domain([0, 100, 500])
+  .range([0, 50, 100])
+  // .clamp(true);
+
+console.log(yScale(0));
+console.log(yScale(100));
+console.log(yScale(24000));
+
 d3.select('svg')
   .selectAll('rect')
-  .data([15, 50, 22, 8, 100, 10])
+  .data([14, 68, 24500, 430, 19, 1000, 5555])
   .enter()
   .append('rect')
   .attr('width', 10)
-  .attr('height', d => d)
-  .style('opacity', 0.25)
+  .attr('height', d => yScale(d))
   .attr('x', (d, i) => i * 10)
-  .attr('y', (d, i) => 100 - d);
+  .attr('y', d => 100 - yScale(d))
+  .style('fill', '#FE9922')
+  .style("stroke", "#9A8B7A")
+  .style('stroke-width', '1');
