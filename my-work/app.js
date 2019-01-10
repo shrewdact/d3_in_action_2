ddf86adc8502db44c0abe76ee171da082d591495
JuSeonghyeok <shrewdact@gmail.com>
2019-01-10 09:13:21 +0900

    Done 2.3 loading data, casting it, measuring it, and displaying t as bar chart

--- my-work/app.js
+++ my-work/app.js
@@ -1,24 +1,23 @@
 // d3.csv('../data/cities.csv', data => console.log(data));
+d3.csv('../data/cities.csv', (error, data) => dataViz(data));
 
-var yScale = d3
-  .scaleLinear()
-  .domain([0, 100, 500])
-  .range([0, 50, 100])
-  // .clamp(true);
-
-console.log(yScale(0));
-console.log(yScale(100));
-console.log(yScale(24000));
-
-d3.select('svg')
-  .selectAll('rect')
-  .data([14, 68, 24500, 430, 19, 1000, 5555])
-  .enter()
-  .append('rect')
-  .attr('width', 10)
-  .attr('height', d => yScale(d))
-  .attr('x', (d, i) => i * 10)
-  .attr('y', d => 100 - yScale(d))
-  .style('fill', '#FE9922')
-  .style("stroke", "#9A8B7A")
-  .style('stroke-width', '1');
+function dataViz(incomingData) {
+  var maxPopulation = d3.max(incomingData, d => parseInt(d.population));
+  var yScale = d3
+    .scaleLinear()
+    .domain([0, maxPopulation])
+    .range([0, 460]);
+  d3.select('svg').attr('style', 'height: 480px; width: 600px;');
+  d3.select('svg')
+    .selectAll('rect')
+    .data(incomingData)
+    .enter()
+    .append('rect')
+    .attr('width', 50)
+    .attr('height', d => yScale(parseInt(d.population)))
+    .attr('x', (d, i) => i * 60)
+    .attr('y', d => 480 - yScale(parseInt(d.population)))
+    .style('fill', '#FE9922')
+    .style('stroke', '#9A8B7A')
+    .style('stroke-width', '1px');
+}
