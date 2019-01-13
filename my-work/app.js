e5bf9ac0a136bdfb0e208ea554d1da3247baa8ce
JuSeonghyeok <shrewdact@gmail.com>
2019-01-13 13:53:13 +0900

    Done 4.3 complex objects figure 4.4 .each of the boxplot five elements

--- my-work/app.js
+++ my-work/app.js
@@ -37,17 +37,6 @@ function scatterplot(data) {
     .call(xAxis);
 
   d3.select('svg')
-    .selectAll('circle.median')
-    .data(data)
-    .enter()
-    .append('circle')
-    .attr('class', 'tweets')
-    .attr('r', 5)
-    .attr('cx', d => xScale(d.day))
-    .attr('cy', d => yScale(d.median))
-    .style('fill', 'darkgray');
-
-  d3.select('svg')
     .selectAll('g.box')
     .data(data)
     .enter()
@@ -59,8 +48,53 @@ function scatterplot(data) {
     )
     .each(function(d, i) {
       d3.select(this)
+        .append('line')
+        .attr('class', 'range')
+        .attr('x1', 0)
+        .attr('x2', 0)
+        .attr('y1', yScale(d.max) - yScale(d.median))
+        .attr('y2', yScale(d.min) - yScale(d.median))
+        .style('stroke', 'black')
+        .style('stroke-width', '4px');
+
+      d3.select(this)
+        .append('line')
+        .attr('class', 'max')
+        .attr('x1', -10)
+        .attr('x2', 10)
+        .attr('y1', yScale(d.max) - yScale(d.median))
+        .attr('y2', yScale(d.max) - yScale(d.median))
+        .style('stroke', 'black')
+        .style('stroke-width', '4px');
+
+      d3.select(this)
+        .append('line')
+        .attr('class', 'min')
+        .attr('x1', -10)
+        .attr('x2', 10)
+        .attr('y1', yScale(d.min) - yScale(d.median))
+        .attr('y2', yScale(d.min) - yScale(d.median))
+        .style('stroke', 'black')
+        .style('stroke-width', '4px');
+
+      d3.select(this)
         .append('rect')
+        .attr('class', 'range')
         .attr('width', 20)
-        .attr('height', yScale(d.q1) - yScale(d.q3));
+        .attr('x', -10)
+        .attr('y', yScale(d.q3) - yScale(d.median))
+        .attr('height', yScale(d.q1) - yScale(d.q3))
+        .style('fill', 'white')
+        .style('stroke', 'black')
+        .style('stroke-width', '2px');
+
+      d3.select(this)
+        .append('line')
+        .attr('x1', -10)
+        .attr('x2', 10)
+        .attr('y1', 0)
+        .attr('y2', 0)
+        .style('stroke', 'darkgray')
+        .style('stroke-width', '4px');
     });
 }
