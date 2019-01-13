d14fb9b75f2493b98bf7f3ec076117314f561996
JuSeonghyeok <shrewdact@gmail.com>
2019-01-13 14:27:54 +0900

    Done 4.4 line charts and interpolations listing 4.7 draw scatterplot tweetdata

--- my-work/app.js
+++ my-work/app.js
@@ -1,102 +1,69 @@
-d3.csv('../data/boxplot.csv', scatterplot);
+d3.csv('../data/tweetdata.csv', lineChart);
 
-function scatterplot(data) {
-  const tickSize = 470;
+function lineChart(data) {
+  const blue = '#5eaec5',
+    green = '#92c463',
+    orange = '#fe9a22';
   const xScale = d3
     .scaleLinear()
-    .domain([1, 8])
-    .range([20, tickSize]);
-
+    .domain([1, 10, 5])
+    .range([20, 480]);
   const yScale = d3
     .scaleLinear()
-    .domain([0, 100])
-    .range([tickSize + 10, 20]);
+    .domain([0, 35])
+    .range([480, 20]);
+  const xAxis = d3
+    .axisBottom()
+    .scale(xScale)
+    .tickSize(480)
+    .tickValues([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+  d3.select('svg')
+    .append('g')
+    .attr('id', 'xAxisG')
+    .call(xAxis);
 
   const yAxis = d3
     .axisRight()
     .scale(yScale)
-    .ticks(8)
-    .tickSize(-tickSize);
+    .ticks(10)
+    .tickSize(480);
 
   d3.select('svg')
     .append('g')
-    .attr('transform', 'translate(470,0)')
     .attr('id', 'yAxisG')
     .call(yAxis);
 
-  const xAxis = d3
-    .axisBottom()
-    .scale(xScale)
-    .tickSize(-tickSize)
-    .tickValues([1, 2, 3, 4, 5, 6, 7]);
-
   d3.select('svg')
-    .append('g')
-    .attr('transform', `translate(0,${tickSize})`)
-    .attr('id', 'xAxisG')
-    .call(xAxis);
-  d3.select('#xAxisG > path.domain').style('display', 'none')
-
-  d3.select('svg')
-    .selectAll('g.box')
+    .selectAll('circle.tweets')
     .data(data)
     .enter()
-    .append('g')
-    .attr('class', 'box')
-    .attr(
-      'transform',
-      d => 'translate(' + xScale(d.day) + ',' + yScale(d.median) + ')'
-    )
-    .each(function(d, i) {
-      d3.select(this)
-        .append('line')
-        .attr('class', 'range')
-        .attr('x1', 0)
-        .attr('x2', 0)
-        .attr('y1', yScale(d.max) - yScale(d.median))
-        .attr('y2', yScale(d.min) - yScale(d.median))
-        .style('stroke', 'black')
-        .style('stroke-width', '4px');
+    .append('circle')
+    .attr('class', 'tweets')
+    .attr('r', 5)
+    .attr('cx', d => xScale(d.day))
+    .attr('cy', d => yScale(d.tweets))
+    .style('fill', blue);
 
-      d3.select(this)
-        .append('line')
-        .attr('class', 'max')
-        .attr('x1', -10)
-        .attr('x2', 10)
-        .attr('y1', yScale(d.max) - yScale(d.median))
-        .attr('y2', yScale(d.max) - yScale(d.median))
-        .style('stroke', 'black')
-        .style('stroke-width', '4px');
+  d3.select('svg')
+    .selectAll('circle.retweets')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('class', 'retweets')
+    .attr('r', 5)
+    .attr('cx', d => xScale(d.day))
+    .attr('cy', d => yScale(d.retweets))
+    .style('fill', green);
 
-      d3.select(this)
-        .append('line')
-        .attr('class', 'min')
-        .attr('x1', -10)
-        .attr('x2', 10)
-        .attr('y1', yScale(d.min) - yScale(d.median))
-        .attr('y2', yScale(d.min) - yScale(d.median))
-        .style('stroke', 'black')
-        .style('stroke-width', '4px');
-
-      d3.select(this)
-        .append('rect')
-        .attr('class', 'range')
-        .attr('width', 20)
-        .attr('x', -10)
-        .attr('y', yScale(d.q3) - yScale(d.median))
-        .attr('height', yScale(d.q1) - yScale(d.q3))
-        .style('fill', 'white')
-        .style('stroke', 'black')
-        .style('stroke-width', '2px');
-
-      d3.select(this)
-        .append('line')
-        .attr('x1', -10)
-        .attr('x2', 10)
-        .attr('y1', 0)
-        .attr('y2', 0)
-        .style('stroke', 'darkgray')
-        .style('stroke-width', '4px');
-
-    });
+  d3.select('svg')
+    .selectAll('circle.favorites')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('class', 'favorites')
+    .attr('r', 5)
+    .attr('cx', d => xScale(d.day))
+    .attr('cy', d => yScale(d.favorites))
+    .style('fill', orange);
 }
